8dd96d9356d2b4dceeb0045be40c9e6c7b31b6fa
kyemelianov <kyemelianov@lohika.com>
2018-05-19 18:05:39 +0300

    update maven structure

--- src/main/java/patterns/observer/ForecastDisplay.java
+++ src/main/java/patterns/observer/ForecastDisplay.java
@@ -13,10 +13,12 @@ public class ForecastDisplay implements Observer, DisplayElement {
     weatherData.registerObserver(this);
   }
 
+  @Override
   public void display() {
     System.out.println("Forecast conditions: " + pressure + " pressure, humidity: " + humidity);
   }
 
+  @Override
   public void update(float temperature, float humidity, float pressure) {
     this.temperature = temperature;
     this.humidity = humidity;
